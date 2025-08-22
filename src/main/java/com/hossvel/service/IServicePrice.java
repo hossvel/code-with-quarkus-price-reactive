ed0828ed9a9d6b3514d82b64bbb12940b01ec2c1
devhoss <velasco2288@gmail.com>
2025-08-22 12:24:11 -0500

    getallprice

--- src/main/java/com/hossvel/service/IServicePrice.java
+++ src/main/java/com/hossvel/service/IServicePrice.java
@@ -7,6 +7,6 @@ import io.smallrye.mutiny.Uni;
 import java.util.List;
 
 public interface IServicePrice {
-     Uni<List<Price>> getAll();
+     Uni<List<Price>> getHistoryPrice();
     public List<Price> getHistoryById(Long product_id);
 }
